5054af14c7bccdec63f831e5e81b852c92685846
Venkatakrishnan Ganesh <venkat@automorphic-software.com>
2012-04-04 02:04:01 +0530

    * Added stub templates and contollers

--- lib/application.py
+++ lib/application.py
@@ -1,5 +1,7 @@
 
 from model import setting, user, content, entry, tag, tagmap
+from google.appengine.ext.webapp import template
+import os, os.path
 
 
 class Application( object ):
@@ -13,6 +15,19 @@ class Application( object ):
   @classmethod
   def has_been_setup( cls ):
     return setting.Setting.is_app_setup()
+
+  # FIXME: Duplication
+  @staticmethod
+  def include( f, d ):
+
+    _path = os.path.join(
+      os.path.dirname( __file__ ),
+      '..', 'themes', 'admin', f
+    )
+
+    print _path
+
+    return template.render( _path, { } )
 
   @classmethod
   def themes( cls ):
--- lib/handlers/admin/index.py
+++ lib/handlers/admin/index.py
@@ -1,9 +1,47 @@
 
 from util.handler import Handler
-import base
+import base, application
 
 
 class AdminHandler( base.Base ):
 
   def get( self ):
-    self.response.out.write( "Woohoo! This is the admin panel" )
+    allowed = [
+      'dashboard',
+      'settings',
+      'new',
+      'all'
+    ]
+
+    if self.parts()[1] in allowed:
+      self.__getattribute__( self.parts()[1] )()
+    else:
+      # FIXME: To be implemented
+      # self.not_found()
+      pass
+
+  # Dispatch posts to get too
+  # that dispatches it to other places anyway
+  def post( self ):
+    self.get()
+
+  def _default_params( self ):
+    return {
+      "show_top_bar": True,
+      "include": application.Application.include
+    }
+
+  # FIXME: Stub implementations
+  def dashboard( self ):
+    self.render_template( 'dashboard.html', self._default_params() )
+
+  def all( self ):
+    self.render_template( 'all.html', self._default_params() )
+
+  def settings( self ):
+    self.render_template( 'settings.html', self._default_params() )
+
+  def new( self ):
+    self.render_template( 'new.html', self._default_params() )
+
+
